70bbb28c6a4d0a8e48f4e97aff7b480083ef122f
Andreas Sorteberg Vik <256838@stud.inn.no>
2021-11-05 18:21:09 +0100

    Made better

--- Connect 4/main.cpp
+++ Connect 4/main.cpp
@@ -1,11 +1,23 @@
 #include "declear.h"
 
 int main() {
-	std::cout << "If you want to play against, make player 2's name 'AI'" << std::endl;
-	std::cout << "Player 1 name: ";
-	getline(std::cin, playerOneName);
-	std::cout << "Player 2 name: ";
-	getline(std::cin, playerTwoName);
+	while (true) {
+		std::cout << "If you want to play against, make player 2's name 'AI'" << std::endl;
+		std::cout << "Player 1 name: ";
+		getline(std::cin, playerOneName);
+		std::cout << "Player 2 name: ";
+		getline(std::cin, playerTwoName);
+
+		//Makes sure name cannot be the same, so player will not get confused as to which turn it is
+		if (playerOneName == playerTwoName) {
+			system("cls");
+			std::cout << "You cannot have the same name. Please try again." << std::endl;
+			continue;
+		}
+		else {
+			break;
+		}
+	}
 
 	if (playerTwoName == "AI" || playerTwoName == "ai" || playerTwoName == "aI" || playerTwoName == "Ai") {
 		system("cls");
@@ -21,15 +33,34 @@ int main() {
 }
 
 void playerGame() {
+	player1Play();
+
+	return;
+}
+
+void player1Play() {
 	while (player1 == true) {
+		std::cout << "It is time for '" << playerOneName << "' to take their turn." << std::endl;
 		emptyBoard();
+
+		input = _getch();
+
+		switch (input) {
+		case 1:
+			//Placement == board[6][1]
+		}
 		
+		break;
 	}
 
 	return;
 }
 
-void player1Play();
+void player2Play() {
+
+
+	return;
+}
 
 void AIGame() {
 	std::cout << "AI Gameplay";
@@ -39,11 +70,13 @@ void AIGame() {
 
 void emptyBoard() {
 	std::cout << std::endl;
+	std::cout << numbers << std::endl;
 	for (int i = 0; i < row; i++) {
+		std::cout << "|";
 		for (int j = 0; j < col; j++) {
-			std::cout << board[i][j];
+			std::cout << " " << board[i][j];
 		}
-		std::cout << std::endl;
+		std::cout << " |" << std::endl;
 	}
 
 	return;
